cf00c2c3a1b0fa766e550df9d65159adf63791e6
psangwoo <qkrtkddn130@naver.com>
2022-08-22 13:28:40 +0900

    Wasm -> knstld>

--- app/app.go
+++ app/app.go
@@ -107,8 +107,8 @@ const appName = "WasmApp"
 
 // We pull these out so we can set them with LDFLAGS in the Makefile
 var (
-	NodeDir      = ".wasmd"
-	Bech32Prefix = "wasm"
+	NodeDir      = ".knstld"
+	Bech32Prefix = "darc"
 
 	// If EnabledSpecificProposals is "", and this is "true", then enable all x/wasm proposals.
 	// If EnabledSpecificProposals is "", and this is not "true", then disable all x/wasm proposals.
